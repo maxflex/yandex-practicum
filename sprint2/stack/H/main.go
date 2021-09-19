2a5cabb163065f145c03dbd563f316c5b9e99328
maxflex <makcyxa-k@yandex.ru>
2021-09-19 18:59:38 +0300

    Sprint 2, H

---
+++ sprint2/stack/H/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s Stack
+	var seq string
+	opening := map[rune]rune{
+		'{': '}',
+		'[': ']',
+		'(': ')',
+	}
+	closing := map[rune]rune{
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+	fmt.Scan(&seq)
+	for _, i := range seq {
+		if _, ok := opening[i]; ok {
+			s.push(i)
+		} else if _, ok := closing[i]; ok {
+			popped, err := s.pop()
+			if err != nil || popped != closing[i] {
+				fmt.Print("False")
+				return
+			}
+		}
+	}
+	fmt.Print("True")
+}
---
+++ sprint2/stack/H/stack.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+)
+
+type Stack struct {
+	items []rune
+}
+
+func (s *Stack) push(x rune) {
+	s.items = append(s.items, x)
+}
+
+func (s *Stack) pop() (rune, error) {
+	if len(s.items) == 0 {
+		return -1, errors.New("empty pussy stack")
+	}
+	popped := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return popped, nil
+}
